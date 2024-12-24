0c8f43520430194bde7b108be25d98040320f953
sheiely <sheielynascimento@gmail.com>
2024-12-23 21:50:03 -0300

    fix: correção de diretorios

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,34 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+
+class ActionComoFuncionaAuxilio(Action):
+    def name(self) -> str:
+        return "action_como_funciona_auxilio"
+
+    def run(self, dispatcher, tracker, domain):
+        # Recuperando o valor do slot 'auxilio-bolsa'
+        print("to akiiii")
+        auxilio = tracker.get_slot("auxilio-bolsa")
+        
+        # Lógica para determinar qual resposta usar com base no valor do slot
+        if auxilio == "PIBIC":
+            dispatcher.utter_template("utter_como_funciona_pibic", tracker)
+        elif auxilio == "PIBIT":
+            dispatcher.utter_template("utter_como_funciona_pibit", tracker)
+        elif auxilio == "PID":
+            dispatcher.utter_template("utter_como_funciona_pid", tracker)
+        elif auxilio == "BIA":
+            dispatcher.utter_template("utter_como_funciona_bia", tracker)
+        elif auxilio == "emergencial":
+            dispatcher.utter_template("utter_como_funciona_aux_emergencial", tracker)
+        elif auxilio == "creche":
+            dispatcher.utter_template("utter_como_funciona_aux_creche", tracker)
+        elif auxilio == "moradia":
+            dispatcher.utter_template("utter_como_funciona_aux_moradia", tracker)
+        else:
+            dispatcher.utter_message(text="Desculpe, não reconheci o tipo de bolsa ou auxílio. Pode me dar mais informações?")
+        
+        return []
